971fed7b558249cf658aabef68a820a611b4c00b
ranjith <ranjithkumar180205@gmail.com>
2026-09-01 17:09:18 +0530

    create engine ,session local,base and dependency

---
+++ backend/app/databasee.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./evals.db")
+
+#create engine and session local
+engine = create_engine(DATABASE_URL,connect_args = {"check_same_thread":False})
+SessionLocal = sessionmaker(autocommit =False,autoflush=False,bind =engine)
+
+
+#create base and dependency
+Base =declarative_base()
+def get_db():
+    try:
+        yield Base
+    finally:
+        Base.close()
